f7636b3628502b9dd20b5888d2863d725ada8619
Nahyun <from.owol@gmail.com>
2023-11-15 11:58:06 +0900

    test: 주문 내역을 문자열로 반환하는 테스트 추가

--- __tests__/OrderTest.js
+++ __tests__/OrderTest.js
@@ -1,7 +1,9 @@
 import { KINDS, MENUS } from '../src/constants/menus.js';
-import { SYMBOL } from '../src/constants/texts.js';
+import { OUTPUT_MESSAGE } from '../src/constants/texts.js';
 
 describe('Order', () => {
+  let orderSheet = new Map();
+
   test.each([
     ['초코케이크', KINDS.dessert],
     ['양송이수프', KINDS.appetizer],
@@ -15,23 +17,33 @@ describe('Order', () => {
     }
   });
 
-  test('주문 내역 배열을 입력하면, 메뉴 이름과 개수를 분리한다.', () => {
+  test('메뉴를 입력하면, 메뉴 이름과 개수로 분리한다.', () => {
     const orders = ['타파스-2', '샴페인-1'];
-    let orderSheet = new Map();
 
     orders.forEach(order => {
-      const [menuName, quantity] = order.split(SYMBOL.bar);
+      const [menuName, quantity] = order.split('-');
       orderSheet.set(menuName, Number(quantity));
     });
 
     expect(orderSheet.get('타파스')).toBe(2);
   });
 
-  test('주문 내역 배열을 입력하면, 총주문 금액을 계산한다.', () => {
-    const orderSheet = new Map([
-      ['타파스', 2],
-      ['샴페인', 1],
-    ]);
+  test.each([['타파스 2개'], ['샴페인 1개']])(
+    '주문 내역 Map의 메뉴 이름과 개수를 문자열로 묶어 반환한다.',
+    order => {
+      let orderDetails = '';
+
+      orderSheet.forEach((count, menuName) => {
+        if (count !== 0) {
+          orderDetails += OUTPUT_MESSAGE.menus(menuName, count);
+        }
+      });
+
+      expect(orderDetails).toContain(order);
+    },
+  );
+
+  test('주문 내역 Map으로 총주문 금액을 계산한다.', () => {
     let totalAmount = 0;
 
     orderSheet.forEach((quantity, menuName) => {
@@ -43,7 +55,7 @@ describe('Order', () => {
     expect(totalAmount).toBe(36000);
   });
 
-  test('주문 내역 배열에 디저트 메뉴가 있으면, 디저트 메뉴의 개수를 반환한다.', () => {
+  test('주문 내역에 디저트 메뉴가 있으면, 디저트 메뉴의 개수를 반환한다.', () => {
     const orderSheet = new Map([
       ['초코케이크', 1],
       ['아이스크림', 1],
